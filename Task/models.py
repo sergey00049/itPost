83f4e460ccaa814a60354ede265c84dfdeb27c33
gray00049 <gray00049@bk.ru>
2020-01-07 17:42:39 +0300

    ..

--- Task/models.py
+++ Task/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     title = models.CharField(max_length=200, default="Новая заметка")
     text = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.title
